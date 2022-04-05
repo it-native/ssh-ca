074dde67a94ddd27ab462d5817b1de0f96d76404
Chris Pratl <git@pratl.net>
2022-04-05 18:23:05 +0000

    Initial commit

---
+++ cert.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# chdir to the right working directory
+cd /root/ssh-ca
+
+# Get hostname of the server
+server=$1
+
+# Check if the server hostname is given - otherwise, exit
+if [[ $server == "" ]]
+then
+	echo Please specify a host.
+	exit 1
+fi
+
+# Get the serial number
+serial=$(cat serial)
+
+# change directory to the server
+cd $server
+
+# Find infile
+for infile in id_rsa id_ed25519 ssh_host_rsa_key ssh_host_ecdsa_key ssh_host_ed25519_key
+do
+	if [[ -f $infile.pub ]]
+	then
+		break
+	fi
+done
+if [[ ! -f $infile.pub ]]
+then
+	echo No public key file found. The script does not work now.
+	exit
+fi
+if [[ $infile == ssh_host* ]]
+then
+	command="ssh-keygen -h"
+else
+	command=ssh-keygen
+fi
+
+# Check if principals are given
+if [[ -f principals ]]
+then
+	principals=$(cat principals)
+else
+	principals=$server
+fi
+
+# Create the certificate
+$command \
+	-s ../ca \
+	-I $server \
+	-V +40d \
+	-z $serial \
+	-n $principals \
+	$infile.pub
+
+mv $infile-cert.pub current-cert.pub
+
+# Update serial number
+cd ..
+serial=$(($serial+1))
+echo $serial > serial
