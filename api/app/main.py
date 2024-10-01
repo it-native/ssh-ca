9da5a87526177ace353fe3155eede3135d4ca00b
Chris <chris@pratl.net>
2024-10-01 13:05:23 +0200

    Minor fix

--- api/app/main.py
+++ api/app/main.py
@@ -8,6 +8,8 @@ from typing import Union
 import subprocess
 
 from fastapi import FastAPI, Response
+from fastapi.responses import PlainTextResponse
+
 from . import schemas
 
 app: FastAPI = FastAPI()
@@ -23,7 +25,7 @@ app: FastAPI = FastAPI()
                 "You did something nasty. Refer to the "
                 "message to find out what went wrong."
             ),
-            "model": schemas.Error
+            "model": str
         },
         404: {
             "description": (
@@ -32,14 +34,15 @@ app: FastAPI = FastAPI()
                 "in a later release; you will currently have to "
                 "do it by hand."
             ),
-            "model": schemas.Error
+            "model": str
         }
-    }
+    },
+    response_class=PlainTextResponse
 )
 async def sign_certificate(
     name: str,
     response: Response,
-):
+) -> str:
     """
         Check if this certificate name exists and if so,
         sign it using the cert.sh script.
@@ -55,15 +58,11 @@ async def sign_certificate(
     ]:
         # Disallowed names.
         response.status_code = 403
-        return schemas.Error(
-            message=f"{name} is not allowed."
-        )
+        return f"{name} is not allowed."
 
     if not os.path.isdir(name):
         response.status_code = 404
-        return schemas.Error(
-            message=f"{name} does not exist."
-        )
+        return f"{name} does not exist."
 
     subprocess.call([
         "./cert.sh",
